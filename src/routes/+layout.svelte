ddd3573f2fc7526903e37424d2d78df6459f5592
njdra-casey <160993424+njdra-casey@users.noreply.github.com>
2024-10-10 22:29:16 +0800

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,33 @@
 <script>
+    import "../app.css";
     import Footer from "../routes/components/Footer.svelte";
     import Header from "../routes/components/Header.svelte";
-    import "../app.css";
-    let y;
+
+    let y = 0;
     let innerWidth = 0;
     let innerHeight = 0;
 
     function goTop() {
         document.body.scrollIntoView();
     }
-</script>
+  
+  </script>
+  <div 
+  class="relative flex flex-col max-w-[1900px] max-auto w-full text-sm sm:text-base min-h-screen"
+  >
+  <div class={"fixed botton-0 w-full duration-200 flex p-10 z-[10] " + 
+   ( y > 0 
+        ? "opacity-full pointer-events-auto" 
+        : "pointer-events-none opacity-0")}
+  > 
+        <button on:click={goTop} class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer ">
 
-<div
-    class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
-    >
-    <div
-        class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
-            (y > 0
-                ? " opacity-full pointer-events-auto"
-                : " pointer-events-none opacity-0")}
-    >
-        <button
-            on:click={goTop}
-            class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
-        >
-            <i class="fa-solid fa-arrow-up" />
+          <i class="fa-solid fa-arrow-up grid place-items-center aspect-aquare" />
         </button>
-    </div>
-    <Header {y} {innerHeight}/>
-    <slot />
-    <Footer />
+   </div>
+   <Header {y} />
+   <slot />
+   <Footer />
 </div>
+
 <svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
