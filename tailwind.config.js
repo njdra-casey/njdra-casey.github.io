681e57e6289d38d1b1d5e49e51943850a4f69295
nojadera <nojadera>
2024-11-02 22:33:09 +0800

    MINOR CHANGES

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
